90ccf345fe7e30deec26989edd171abc37d557b7
Little-Pa <144716118+Little-Pa@users.noreply.github.com>
2023-11-09 16:35:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-    def main():
-"""
-##################################################
-# Comlete your code here
-Use the same variables: celsius fahrenheit 
-##################################################
-"""
-celsius = int(input('enter your temperature in Celsius: '))
-    fahrenheit = 9 / 5 * celsius + 32
+        def main():
+    """
+    ##################################################
+    # Comlete your code here
+    Use the same variables: celsius fahrenheit 
+    ##################################################
+    """
+        celsius = int(input('enter your temperature in Celsius: '))
+            fahrenheit = 9 / 5 * celsius + 32
+            
+            print ('the converted temperature is ', fahrenheit)
+    """
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    """
+    return celsius, fahrenheit
     
-    print ('the converted temperature is ', fahrenheit)
-"""
-########################################
-# Do not delete the return statement
-########################################
-"""
-return celsius, fahrenheit
-
-
-    if __name__ == '__main__':
-        main()
+    
+        if __name__ == '__main__':
+            main()
